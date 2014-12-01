5d4f2255d6738e057f1b201e2bda5fd73b6a4d6c
Jeff <Jffrypwll@googlemail.com>
2014-12-01 19:06:01 +0000

    Update update_rrd_db.php

--- update_rrd_db.php
+++ update_rrd_db.php
@@ -4,28 +4,13 @@
   // Fetch current time
   $now = time();
 
-  $data=snmp2_walk("roost-vgw", "Rakuten", "1.3.6.1.4.1.9.9.63.1.3.2.1.1");
+  $data=snmp2_walk("server", "community", "1.3.6.1.4.1.9.9.63.1.3.2.1.1");
 
-#  $data=$(snmpwalk -v 2c -c Rakuten roost-vgw 1.3.6.1.4.1.9.9.63.1.3.2.1.1 | grep Hex-STRING | wc -l);
-    
-#  $ret = rrd_update("jcall.rrd", "$now:$data");
-echo "$data";
+  $lines = preg_grep( "/Hex-STRING/", $data );
 
-foreach ($data as $d){
-  echo "-$d\n";
-}
+  $calls = count($lines);
 
-$lines = preg_grep( "/Hex-STRING/", $data );
-
-$calls = count($lines);
-
-echo "$now,$calls";
-
-$ret = rrd_update("/var/scripts/jcall/jcall.rrd", "$now:$calls");
-
-echo "-$ret-3-";
-
-echo $ret;
+  $ret = rrd_update("/var/scripts/jcall/jcall.rrd", "$now:$calls");
 
 ?>
 
