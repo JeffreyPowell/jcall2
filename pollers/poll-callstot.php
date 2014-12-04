03519125a3c610139e8ed404d613d7774da77184
Jeff <Jffrypwll@googlemail.com>
2014-12-04 17:30:57 +0000

    Update poll-callstot.php

--- pollers/poll-callstot.php
+++ pollers/poll-callstot.php
@@ -3,7 +3,7 @@
   // Fetch current time
   $now = time();
 
-  $data=snmp2_walk("roost-vgw", "Rakuten", "1.3.6.1.4.1.9.9.63.1.3.2.1.1");
+  $data=snmp2_walk("roost-vgw", "xxx", "1.3.6.1.4.1.9.9.63.1.3.2.1.1");
 
   $lines = preg_grep( "/Hex-STRING/", $data );
 
